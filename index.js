c6f87d7d711c9b384c4108d52a7c44beb58454d6
Rodrigo <rodrigoaugusto2103@gmail.com>
2023-01-12 13:08:29 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,93 @@
+const texto = document.querySelector('input')
+const btnInsert = document.querySelector('.divInsert button')
+const btnDeleteAll = document.querySelector('.header button')
+const ul = document.querySelector('ul')
+const d = new Date()
+document.getElementById("date").innerHTML = d;
+
+var itensDB = []
+
+btnDeleteAll.onclick = () => {
+  itensDB = []
+  updateDB()
+}
+
+texto.addEventListener('keypress', e => {
+  if (e.key == 'Enter' && texto.value != '') {
+    setItemDB()
+  }
+})
+
+btnInsert.onclick = () => {
+  if (texto.value != '') {
+    setItemDB()
+  }
+}
+
+function setItemDB() {
+  if (itensDB.length >= 20) {
+    alert('Limite máximo de 20 itens atingido!')
+    return
+  }
+
+  itensDB.push({'item': texto.value, 'status': '' })
+  updateDB()
+}
+
+function updateDB() {
+  localStorage.setItem('todolist', JSON.stringify(itensDB))
+  loadItens()
+}
+
+function loadItens() {
+  ul.innerHTML = "";
+  itensDB = JSON.parse(localStorage.getItem('todolist')) ?? []
+
+  itensDB.forEach((item,i) => {
+    insertItemTela(item.item, item.status, i)
+  });
+}
+
+function insertItemTela(text, status, i) {
+  const li = document.createElement('li')
+  li.innerHTML = `
+    <div class="divLi">
+      <input type="checkbox" ${status} data-i=${i} onchange="done(this, ${i});" />
+      <span data-si=${i}>${text}</span>
+      <button onclick = "editarItem(${i})" data-i= ${i}><i class = 'bx bx-pencil'></i></button>
+      <button onclick="removeItem(${i})" data-i=${i}><i class='bx bx-trash'></i></button>
+    </div>
+    `
+  ul.appendChild(li)
+
+  if (status) {
+    document.querySelector(`[data-si="${i}"]`).classList.add('line-through')
+  } else {
+    document.querySelector(`[data-si="${i}"]`).classList.remove('line-through')
+  }
+
+  texto.value = ''
+}
+
+function done(chk, i) {
+
+  if (chk.checked) {
+    itensDB[i].status = 'checked'
+  } else {
+    itensDB[i].status = '' 
+  }
+
+  updateDB()
+}
+
+function removeItem(i) {
+  itensDB.splice(i, 1)
+  updateDB()
+}
+
+function editarItem(i){
+  itensDB[i].item = prompt("Edit your item")
+  updateDB()
+}
+
+loadItens()
